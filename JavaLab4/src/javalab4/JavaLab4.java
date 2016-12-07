e6af2b8127f252278049d2e549cb5dc0d8e67320
Мария <fulgentuniverse@gmail.com>
2016-12-07 21:32:14 +0500

    finish lab 4

--- JavaLab4/src/javalab4/JavaLab4.java
+++ JavaLab4/src/javalab4/JavaLab4.java
@@ -7,21 +7,23 @@ public class JavaLab4 {
    */
   public static void main(String[] args) {
     
-    BusinessCenter bC = new BusinessCenter();
-    Visitor vis1 = new Visitor(bC);
-    Visitor vis2 = new Visitor(bC);
-    Visitor vis3 = new Visitor(bC);
-    Visitor vis4 = new Visitor(bC);
+    BusinessCenter control = new BusinessCenter();
+    BusinessCenter lift = new BusinessCenter();
+    Visitor vis1 = new Visitor(control, lift);
+    Visitor vis2 = new Visitor(control, lift);
+    Visitor vis3 = new Visitor(control, lift);
+    Visitor vis4 = new Visitor(control, lift);
+    Visitor vis5 = new Visitor(control, lift);
     Thread th1 = new Thread(vis1);
     Thread th2 = new Thread(vis2);
     Thread th3 = new Thread(vis3);
     Thread th4 = new Thread(vis4);
+    Thread th5 = new Thread(vis5);
     th1.start();
     th2.start();
     th3.start();
     th4.start();
-    //SimpleRunnable r = new SimpleRunnable();
-    //r.max = 100;
+    th5.start();
   }
   
 }
